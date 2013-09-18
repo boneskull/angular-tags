c5e24612878d6c55e5c3889f7343f944a3da7340
Christopher Hiller <chiller@badwing.com>
2013-09-17 22:52:40 -0700

    documented stuff

--- tags.js
+++ tags.js
@@ -5,11 +5,15 @@
   var tags = angular.module('decipher.tags', ['ui.bootstrap.typeahead']);
 
   var defaultOptions = {
-      delimiter: ',',
-      classes: {},
-      orderBy: 'name'
+      delimiter: ',', // if given a string model, it splits on this
+      classes: {}, // obj of group names to classes
+      orderBy: 'name' // what we order the tags and typeahead by
     },
+
+    // for parsing comprehension expression
     SRC_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?\s+for\s+(?:([\$\w][\$\w\d]*))\s+in\s+(.*)$/,
+
+    // keycodes
     kc = {
       comma: 188,
       enter: 13,
@@ -20,12 +24,22 @@
     kcRemoveTag = [kc.backspace],
     kcCancelInput = [kc.esc];
 
+  /**
+   * TODO: do we actually share functionality here?  We're using this
+   * controller on both the subdirective and its parent, but I'm not sure
+   * if we actually use the same functions in both.
+   */
   tags.controller('TagsCtrl', function ($scope) {
 
-    //////////////////////
     var deletedSrcTags = [];
 
-    // returns object used by tag's ng-class
+    /**
+     * Figures out what classes to put on the tag span.  It'll add classes
+     * if defined by group, and it'll add a selected class if the tag
+     * is preselected to delete.
+     * @param tag
+     * @returns {{}}
+     */
     $scope.getClasses = function getGroupClass(tag) {
       var r = {};
 
@@ -41,6 +55,11 @@
       return r;
     };
 
+    /**
+     * Adds a tag to the list of tags, and if in the typeahead list,
+     * removes it from that list (and saves it).  emits decipher.tags.added
+     * @param tag
+     */
     $scope.add = function add(tag) {
       var idx;
 
@@ -58,13 +77,18 @@
       });
     };
 
-    // activate typeahead if clicked
-    // if already active - keep the focus on the input
+    /**
+     * Toggle the input box active.
+     */
     $scope.selectArea = function selectArea() {
       $scope.toggles.inputActive = true;
     };
 
-    $scope.selectTag = function selectTag(tag) {
+    /**
+     * Select a tag and emit an event.
+     * @param tag
+     */
+    $scope.select = function select(tag) {
       $scope.toggles.selectedTag = tag;
 
       $scope.$emit('decipher.tags.selected', {
@@ -72,8 +96,12 @@
       });
     };
 
-    // ng-click handler to remove tag
-    $scope.remove = function removeTag(tag) {
+    /**
+     * Removes a tag.  Restores stuff into srcTags if it came from there.
+     * Kills any selected tag.  Emit a decipher.tags.removed event.
+     * @param tag
+     */
+    $scope.remove = function remove(tag) {
       var idx;
       $scope.tags.splice($scope.tags.indexOf(tag), 1);
 
@@ -91,6 +119,10 @@
 
   });
 
+  /**
+   * Directive for the 'input' tag itself, which is of class
+   * decipher-tags-input.
+   */
   tags.directive('decipherTagsInput', function ($timeout, $filter, $rootScope) {
     return {
       restrict: 'C',
@@ -98,10 +130,19 @@
       link: function (scope, element, attrs, ngModel) {
         var delimiterRx = new RegExp('^' + scope.options.delimiter +
                                      '+$'),
+
+          /**
+           * Cancels the text input box.
+           */
           cancel = function cancel() {
             ngModel.$setViewValue('');
             ngModel.$render();
           },
+
+          /**
+           * Adds a tag you typed/pasted in unless it's a bunch of delimiters.
+           * @param value
+           */
           addTag = function addTag(value) {
             if (value) {
               if (value.match(delimiterRx)) {
@@ -114,27 +155,40 @@
               cancel();
             }
           },
+
+          /**
+           * Adds multiple tags in case you pasted them.
+           * @param tags
+           */
           addTags = function (tags) {
             var i;
             for (i = 0; i < tags.length; i++) {
               addTag(tags[i]);
             }
           },
+
+          /**
+           * Backspace one to select, and a second time to delete.
+           */
           removeLastTag = function removeLastTag() {
-            // only do this if the input field is empty.
             var orderedTags;
             if (scope.toggles.selectedTag) {
               scope.remove(scope.toggles.selectedTag);
               delete scope.toggles.selectedTag;
             }
+            // only do this if the input field is empty.
             else if (!ngModel.$viewValue) {
               orderedTags = $filter('orderBy')(scope.tags, scope.orderBy);
               scope.toggles.selectedTag = orderedTags[orderedTags.length - 1];
             }
           };
 
+        /**
+         * When we focus the text input area, drop the selected tag
+         */
         element.bind('focus', function () {
-          // this avoids what looks like a bug in typeahead.
+          // this avoids what looks like a bug in typeahead.  It seems
+          // to be calling element[0].focus() somewhere within a digest loop.
           if ($rootScope.$$phase) {
             delete scope.toggles.selectedTag;
           } else {
@@ -142,24 +196,38 @@
               delete scope.toggles.selectedTag;
             });
           }
-
         });
 
+        /**
+         * Inspects whatever you typed to see if there were character(s) of
+         * concern.
+         */
         element.bind('keyup', function (evt) {
           scope.$apply(function () {
+            // to "complete" a tag
             if (kcCompleteTag.indexOf(evt.keyCode) >= 0) {
               addTag(ngModel.$viewValue);
+
+            // or if you want to get out of the text area
             } else if (kcCancelInput.indexOf(evt.keyCode) >= 0) {
               cancel();
               scope.toggles.inputActive = false;
+
+            // or if you're trying to delete something
             } else if (kcRemoveTag.indexOf(evt.keyCode) >= 0) {
               removeLastTag();
+
+            // otherwise if we're typing in here, just drop the selected tag.
             } else {
               delete scope.toggles.selectedTag;
             }
           });
         });
 
+        /**
+         * When inputActive toggle changes to true, focus the input.
+         * And no I have no idea why this has to be in a timeout.
+         */
         scope.$watch('toggles.inputActive', function (newVal, oldVal) {
           if (newVal !== oldVal) {
             if (newVal) {
@@ -170,6 +238,9 @@
           }
         });
 
+        /**
+         * Detects a paste or someone jamming on the delimiter key.
+         */
         ngModel.$parsers.unshift(function (value) {
           var values = value.split(scope.options.delimiter);
           if (values.length > 1) {
@@ -182,8 +253,10 @@
           return value;
         });
 
+        /**
+         * Resets the input field if we selected something from typeahead.
+         */
         ngModel.$formatters.push(function (tag) {
-          // resets the input if coming from the source tags
           if (tag && tag.value) {
             element.val('');
             return;
@@ -194,6 +267,9 @@
     };
   });
 
+  /**
+   * Main directive
+   */
   tags.directive('tags', function ($document, $timeout, $parse) {
 
     return {
@@ -201,13 +277,21 @@
       restrict: 'E',
       template: '<ng-include data-src="templateUrl"></ng-include>',
       require: 'ngModel',
-      scope: {
-        model: '=ngModel',
-        src: '@'
-      },
+      // we cannot use an isolate scope here due to this issue:
+      // https://github.com/angular/angular.js/issues/1924
+      // either that or I'm too stupid to figure it out
+      scope: true,
       link: function (scope, element, attrs, ngModel) {
         var srcResult,
+          source,
+          locals,
           defaults = angular.copy(defaultOptions),
+
+          /**
+           * Parses the comprehension expression and gives us interesting bits.
+           * @param input
+           * @returns {{itemName: *, source: *, viewMapper: *, modelMapper: *}}
+           */
           parse = function parse(input) {
             var match = input.match(SRC_REGEXP);
             if (!match) {
@@ -225,32 +309,34 @@
 
           };
 
+        // merge options
         scope.options = angular.extend(defaults,
           scope.$eval(attrs.options));
+        // break out orderBy for view
         scope.orderBy = scope.options.orderBy;
 
+        // if we've specified an alternative template, use it.
         scope.templateUrl = scope.options.templateUrl || 'tags.html';
+
+        // this should be named something else since it's just a collection
+        // of random shit.
         scope.toggles = {
           inputActive: false
         };
 
-        // the following two watches are workarounds for this issue:
-        // https://github.com/angular/angular.js/issues/1924
-        // Bring in changes from outside:
-        scope.$watch('model', function () {
-          scope.$eval(attrs.ngModel + ' = model');
-        });
-
-        // Send out changes from inside:
-        scope.$watch(attrs.ngModel, function (val) {
-          scope.model = val;
-        });
-
-        // if we have a string for the model, turn it into an array of objects.
-        // if we have an array of strings, or an array with strings in it,
-        // turn that into an array of objects.
+        /**
+         * if we have a string for the model, turn it into an array of objects.
+         * if we have an array of strings, or an array with strings in it,
+         * turn that into an array of objects.
+         */
         ngModel.$formatters.push(function (value) {
           var arr = [],
+            /**
+             * TODO: replace with something from ngSanitize or $sce
+             * or something.
+             * @param tag
+             * @returns {XML|string}
+             */
             sanitize = function sanitize(tag) {
               return tag
                 .replace(/&/g, '&amp;')
@@ -259,6 +345,7 @@
                 .replace(/'/g, '&#39;')
                 .replace(/"/g, '&quot;');
             };
+
           if (angular.isString(value)) {
             arr = value
               .split(scope.options.delimiter)
@@ -275,7 +362,7 @@
                   name: sanitize(item.trim())
                 };
               }
-              return sanitize(item);
+              return angular.extend(item, {name: sanitize(item.name)});
             })
           }
           else if (angular.isDefined(value)) {
@@ -284,14 +371,26 @@
           return arr;
         });
 
+        /**
+         * This doesn't actually render per se, but what it does is two things:
+         * 1. propagates the (new) value of scope.tags to the parent.  If
+         * you happened to have given it a list of strings, you'll get a list
+         * of objects back instead.
+         * 2. sets scope.tags to be used in the template.
+         */
         ngModel.$render = function $render() {
+          var getter = $parse(attrs.ngModel);
+          getter.assign(scope.$parent, ngModel.$viewValue);
           scope.tags = ngModel.$viewValue;
         };
 
+        // this stuff takes the parsed comprehension expression and
+        // makes a srcTags array full of tag objects out of it.
         scope.srcTags = [];
-        if (scope.src) {
-          srcResult = parse(scope.src);
-          var source = srcResult.source(scope.$parent), locals = {};
+        if (attrs.src) {
+          srcResult = parse(attrs.src);
+          source = srcResult.source(scope.$parent);
+          locals = {};
           if (angular.isDefined(source)) {
             for (var i = 0; i < source.length; i++) {
               locals[srcResult.itemName] = source[i];
