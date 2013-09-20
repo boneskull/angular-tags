c29930f8746621bb225bc583313323df74750d7a
Christopher Hiller <chiller@badwing.com>
2013-09-19 22:09:39 -0700

    testing docs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,36 @@
-console.log('This would be the main JS file.');
+(function () {
+  'use strict';
+
+  angular.module('decipher.tags.github.io', ['decipher.tags'])
+    .controller('MainCtrl', function ($scope) {
+
+      $scope.tags = [
+
+        {
+          name: 'beef', group: 'red'
+        },
+        {
+          name: 'chicken', group: 'white'
+        }
+      ];
+
+      $scope.sourceTags = [
+        {
+          name: 'pork', group: 'red'
+        },
+        {
+          name: 'lamb', group: 'red'
+        },
+        {
+          name: 'turkey', group: 'white'
+        },
+        {
+          name: 'bison', group: 'red'
+        },
+        {
+          name: 'mutton', group: 'red'
+        }
+      ];
+
+    });
+})();
